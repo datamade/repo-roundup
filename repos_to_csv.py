c241950692dd0c2ba707e0734ccc291dbdd0d859
Derek Eder <derek.eder@gmail.com>
2016-12-23 14:43:32 -0600

    updated script to handle new org names and pages

--- repos_to_csv.py
+++ repos_to_csv.py
@@ -1,9 +1,13 @@
 import csv
 import requests
 
-def get_readme_list():
+def get_repo_list():
 
-    api_url = 'https://api.github.com/orgs/datamade/repos?per_page=100'
+    # change these things for your org
+    org_name = 'datamade'
+    pages_to_fetch = 2 # number of repos you have, divided by 100. kinda hack-y!
+
+    api_url = 'https://api.github.com/orgs/%s/repos?per_page=100' % org_name
     repos_list = []
 
     fields = ['name',
@@ -16,7 +20,7 @@ def get_readme_list():
               'created_at',
               'updated_at']
 
-    for page in range(1,3):
+    for page in range(1,(pages_to_fetch + 1)):
         print("Page %s" % page)
         repos = requests.get(api_url + "&page=" + str(page))
         if repos.status_code is 200:
@@ -25,11 +29,14 @@ def get_readme_list():
 
     print("Repo list created. Length: %s" % len(repos_list))
 
-    outp = open('datamade_repos.csv', 'w')
+    outp = open(('%s_repos.csv' % org_name), 'w')
     writer = csv.writer(outp)
     writer.writerow(fields) # header
     writer.writerows(repos_list) # data
     outp.close()
 
+    print("File written: %s" % ('%s_repos.csv' % org_name))
+    print("done")
+
 if __name__ == "__main__":
-    get_readme_list()
+    get_repo_list()
